d2556aa9fac6cb31642bd6e5fef4999d7d18fc8c
fouyer <jfouyer@gmail.com>
2019-10-02 12:41:49 -0300

    introducing: Geo Zones.

--- services/client/application/models/zone.py
+++ services/client/application/models/zone.py
@@ -9,7 +9,6 @@ class Zone(models.Model):
         (0, "Other"),
         (1, "Uber Movement")
     )
-
     type = models.IntegerField(default=0, choices=TYPES, blank=False, null=False)
     name = models.CharField(max_length=127, blank=False, null=False)
     number = models.IntegerField(blank=True, null=False)
