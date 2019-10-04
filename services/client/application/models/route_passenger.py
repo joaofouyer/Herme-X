f82826dcea714334afeebf4c903f9cd5f696ccf2
Fouyer <jfouyer@gmail.com>
2019-10-03 23:46:29 -0300

    ForeignKey in route passenger

--- services/client/application/models/route_passenger.py
+++ services/client/application/models/route_passenger.py
@@ -4,7 +4,7 @@ from django.db import models
 class RoutePassenger(models.Model):
     class Meta:
         ordering = ['pk']
-
+    passenger = models.ForeignKey('Passenger', blank=False, null=False, on_delete=models.CASCADE)
     pick_up = models.ForeignKey('RouteStop', blank=False, null=False, on_delete=models.PROTECT,
                                 related_name='passenger_pickup')
     drop_off = models.ForeignKey('RouteStop', blank=False, null=False, on_delete=models.PROTECT,
